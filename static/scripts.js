b8c3c780ef23825d6d6d379b0131745945b39125
DanteAguero <luisdante727@gmail.com>
2025-12-28 10:02:58 -0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -603,7 +603,9 @@ document.addEventListener("DOMContentLoaded", () => {
       dots.forEach(d => d.classList.remove("active"));
       dot.classList.add("active");
     });
-  
+  });
+});
+
 /* ==========================================================
    BUSCADOR — FIX FINAL (ENTER + MOBILE)
 ========================================================== */
